2593c32c484cf0e126fda8fe5807ab476e519a8f
Admin <20087843@163.com>
2019-10-27 11:25:32 +0800

    add hystrix dashboard to monitor-center

--- custom-services/order-service/src/main/java/cn/pri/smilly/orderservice/client/UserService.java
+++ custom-services/order-service/src/main/java/cn/pri/smilly/orderservice/client/UserService.java
@@ -1,18 +1,30 @@
 package cn.pri.smilly.orderservice.client;
 
 import cn.pri.smilly.commonservice.bean.RestResult;
+import cn.pri.smilly.commonservice.constant.RestStatus;
 import cn.pri.smilly.orderservice.bean.User;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "gateway-service", fallback = UserServiceFallback.class)
+@FeignClient(name = "gateway-service", fallback = UserService.UserServiceFallback.class)
 public interface UserService {
 
     @GetMapping("/user-service/user/info")
-    public RestResult getUserInfo();
+    RestResult getUserInfo();
 
     @PostMapping("/user-service/user/save")
-    public RestResult saveUser(User user);
+    RestResult saveUser(User user);
+
+    @Slf4j
+    class UserServiceFallback implements FallbackFactory<RestResult> {
+        @Override
+        public RestResult create(Throwable throwable) {
+            log.error("user-service dead : " + throwable.getMessage());
+            return RestResult.fail(RestStatus.SVRERR, "user-service dead : " + throwable.getMessage());
+        }
+    }
 
 }
--- monitor-center/src/main/java/cn/pri/smilly/monitorcenter/MonitorCenterApplication.java
+++ monitor-center/src/main/java/cn/pri/smilly/monitorcenter/MonitorCenterApplication.java
@@ -4,10 +4,12 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
+@EnableHystrixDashboard
 public class MonitorCenterApplication {
 
     public static void main(String[] args) {
